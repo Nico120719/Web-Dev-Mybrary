74095a39a82501cd0b70ad7917a06b8b733deaa0
Nico <nicolas.ineksiak@gmail.com>
2021-05-21 21:52:54 -0400

    Commit

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const Author = require('../models/author')
 const router = express.Router()
+const Author = require('../models/author')
 
 // All Authors Route
 router.get('/', async (req, res) => {
